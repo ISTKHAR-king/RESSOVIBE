88d33273927bbbc8ab9e0489f07b9dbe652b24a8
ISTKHAR-king <istkharalam225@gmail.com>
2025-03-22 23:26:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mrhoneyxd07/VAMPIRE_CREW_MUSIC",
+    "https://github.com/ISTKHAR-king/RESSOVIBE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/llVAMPIRE_UPDATEll")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/llVAMPIRE_UPDATEll")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THUNDERDEVS")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+pnDJxCG5VVphMTVl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
